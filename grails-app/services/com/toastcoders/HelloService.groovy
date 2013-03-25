8841272ae4674b27dadfb879aa418e839d1fe2d5
michaelrice <michael@michaelrice.org>
2013-03-25 04:30:54 +0000

    nothing..

--- grails-app/services/com/toastcoders/HelloService.groovy
+++ grails-app/services/com/toastcoders/HelloService.groovy
@@ -12,7 +12,7 @@ class HelloService {
 
     def getMapData() {
         def foo = [:]
-        foo.put("name","mike")
+        foo.put("name","Mike")
         foo.put("age",32)
         foo.put("sex", "yes, often please")
         return foo
